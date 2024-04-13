cedeef147079ff0968a47db6d8990420f97cc8ff
Julia Kardasz <juliakardasz.jk@gmail.com>
2024-04-13 16:50:36 +0200

    działa -tree AVL/BST

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <vector>
 
-#include "min_max.h"
-#include "avl.h"
-#include "bst.h"
-#include "print_tree.h"
-#include "remove_element.h"
-#include "delete_tree.h"
-#include "rebalance_tree.h"
-#include "struct_tree.h"
+// #include "min_max.h"
+// #include "avl.h"
+// #include "bst.h"
+// #include "print_tree.h"
+// #include "remove_element.h"
+// #include "delete_tree.h"
+// #include "rebalance_tree.h"
+// #include "struct_tree.h"
 
-int main(){
-    tree * root;
-
+int main(int argc, char *argv[]){
+    // tree * root;
+    int nodes, pom;
     //dodawanie elementu
     // tree * node;
     // node = new tree;
@@ -20,37 +21,42 @@ int main(){
     // root->right = node;
     // node->prev=root;
 
-    std::string message = "Help\tShow this message\nPrint\tPrint the tree usin In-order, Pre-order, Post-order\nMinMax\tShow min and max element\nRemove\tRemove elements of the tree\nDelete\tDelete whole tree\nExport\tExport the tree to tickzpicture\nRebalance\tRebalance the tree\nExit\tExits the program (same as ctrl+D)\n";
+    std::string tree_type = argv[2];
 
-    std::string action;
-    std::cout << "action> ";
-    std::cin >> action;
-    while(std::cin){
-    if(action == "Help"){
-        std::cout << message;
-    }
-    else if(action == "Print"){
-        std::cout << print_tree(root);
-    }
-    else if(action == "MinMax"){
-        std::cout << min_max(root);
-    }
-    else if(action == "Remove"){
-        std::cout << remove_element(root);
-    }
-    else if(action == "Delete"){
-        std::cout << delete_tree(root);
-    }
-    else if(action == "Rebalance"){
-        std::cout << rebalance_tree(root);
-    }
-     else if(action == "Exit"){
-        return 0;
-    }
-    else std::cout << "Wrong command\n";
-    std::cout << "action> ";
-    std::cin >> action;
-    }
+    //if(tree_type == "AVL") avl(root);
+    // if(tree_type == "BST") bst(root);
+
+    // std::string message = "Help\tShow this message\nPrint\tPrint the tree using In-order, Pre-order, Post-order\nMinMax\tShow min and max element\nRemove\tRemove elements of the tree\nDelete\tDelete whole tree\nExport\tExport the tree to tickzpicture\nRebalance\tRebalance the tree\nExit\tExits the program (same as ctrl+D)\n";
+
+    // std::string action;
+    // std::cout << "action> ";
+    // std::cin >> action;
+    // while(std::cin){
+    // if(action == "Help"){
+    //     std::cout << message;
+    // }
+    // else if(action == "Print"){
+    //     std::cout << print_tree(root);
+    // }
+    // else if(action == "MinMax"){
+    //     std::cout << min_max(root);
+    // }
+    // else if(action == "Remove"){
+    //     std::cout << remove_element(root);
+    // }
+    // else if(action == "Delete"){
+    //     std::cout << delete_tree(root);
+    // }
+    // else if(action == "Rebalance"){
+    //     std::cout << rebalance_tree(root);
+    // }
+    //  else if(action == "Exit"){
+    //     return 0;
+    // }
+    // else std::cout << "Wrong command\n";
+    // std::cout << "action> ";
+    // std::cin >> action;
+    // }
     std::cout << "\n";
     return 0;
 }
