6b2ec046c33926d8d2b0cb4c2142262c1789ca16
Julia Kardasz <juliakardasz.jk@gmail.com>
2024-04-13 23:37:57 +0200

    dodane min i max, powinno dzialac idk

--- src/min_max.cpp
+++ src/min_max.cpp
@@ -6,10 +6,31 @@
 
 #include "../include/struct_tree.h"
 
+int min_element(tree *root){
+    int mini;
+    tree *L = root;
+    if(root->left == NULL) return L->value;
+    while(L->left != NULL){
+        L = L->left;
+    }
+    mini = L->value;
+    return mini;
+}
+
+int max_element(tree *root){
+    int maxi;
+    tree *R = root;
+    if(root->right == NULL) return R->value;
+    while(R->right != NULL){
+        R = R->right;
+    }
+    maxi = R->value;
+    return maxi;
+}
+
 std::string min_max(tree *root){
-    // int mini, maxi;
-    // std::cout << "Min: " << mini << "\n";
-    // std::cout << "Max: " << maxi << "\n";
+    std::cout << "Min: " << min_element(rooti) << "\n";
+    std::cout << "Max: " << max_element(rooti) << "\n";
     std::stringstream resultStream;
     resultStream << "Min and max elements have been found\n";
     return resultStream.str();
