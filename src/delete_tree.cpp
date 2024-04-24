f41e39bc56f976ad02820d04fc83ffd1e1e87bf4
Pawelek <spychala.paw@gmail.com>
2024-04-24 21:37:27 +0200

    dziwny commit możliwe, że coś zepsuję sorki xD

--- src/delete_tree.cpp
+++ src/delete_tree.cpp
@@ -5,6 +5,32 @@
 
 #include "../include/struct_tree.h"
 
-void delete_tree(tree *&root){
-    std::cout << "ok\n";
+void delete_tree2(tree *&root, tree *&copy)
+{
+    if (root)
+    {
+        delete_tree2(root->left, copy); // usuwamy lewe poddrzewo
+        // delete_tree(root->right); // usuwamy prawe poddrzewo
+        if (root != copy)
+        {
+            delete root;
+        } // usuwamy sam węzeł
+    }
 }
+
+void delete_tree(tree *&root)
+{
+    tree *copy = new tree;
+    copy = root;
+    delete_tree2(root, copy);
+}
+
+// void DFSRelease(AVLNode *v)
+// {
+//     if (v)
+//     {
+//         DFSRelease(v->left);  // usuwamy lewe poddrzewo
+//         DFSRelease(v->right); // usuwamy prawe poddrzewo
+//         delete v;             // usuwamy sam węzeł
+//     }
+// }
